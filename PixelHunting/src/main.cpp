32971e1e80f3909bdcc1b0fb5b4a45d485e4850a
Coder246 <tim-arnold@outlook.de>
2020-05-06 09:56:49 +0200

    waitibng for other player

--- PixelHunting/src/main.cpp
+++ PixelHunting/src/main.cpp
@@ -9,20 +9,22 @@
 #define NUMPIXELS 150 // Hier wird die Anzahl der angeschlossenen WS2812 LEDs bzw. NeoPixel angegeben
 Adafruit_NeoPixel led = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-int mypixelx=5;
-int mypixely=5;
-int thierpixelx=6;
-int theirpixely=6;
-boolean amihunter =false;
+int mypixelx = 5;
+int mypixely = 5;
+int thierpixelx = 6;
+int theirpixely = 6;
+boolean amihunter = true;
+boolean ingamewait = false;
 char blue;
 int pixel[10][15];
 
 int row_x[10];
 
-void setup() {
-   led.begin();
+void setup()
+{
+  led.begin();
   Serial.begin(9600);
- Serial1.begin(9600);
+  Serial1.begin(9600);
   row_x[0] = 135;
   row_x[1] = 134;
   row_x[2] = 105;
@@ -52,73 +54,114 @@ void setup() {
   }
 }
 
-void loop() {
+void loop()
+{
 
-  //GET PIXEL THIERSX AND Y and if i hunter
+  //GET PIXEL THIERSX AND Y and if i hunter 
 
-if(Serial1.available()){
-     
-    
-     //blue=Serial1.read();
-     Serial1.setTimeout(50);
-     String bb=Serial1.readString(); 
+  //UND abfrage der eigenen Position falls diese durch eine Zufällige positionierung des esp32 servers verändert wird
 
-      if(bb[0]=='s'){
-blue=bb[1];
-if(blue=='4'){//hoch
-mypixely++;
-}
+  if (Serial1.available()&&!ingamewait)
+  {
 
-if(blue=='2'){//rechts
-if(mypixelx<9) {
-mypixelx++;
-}
-}
+    //blue=Serial1.read();
+    Serial1.setTimeout(50);
+    String bb = Serial1.readString();
 
-if(blue=='0'){//links
-if(mypixelx>0) {
-mypixelx--;
-}
-}
+    if (bb[0] == 's')
+    {
+      blue = bb[1];
+      if (blue == '4')
+      { //hoch
+        if (mypixely < 14)
+        {
+          mypixely++;
+        }
+        else
+        {
+          mypixely = 0;
+        }
+      }
 
-if(blue=='7'){//runter
-mypixely--;
-}
+      if (blue == '2')
+      { //rechts
+        if (mypixelx < 9)
+        {
+          mypixelx++;
+        }
+        else
+        {
+          mypixelx = 0;
+        }
+      }
 
-if(blue=='1'){//neuesspiel
+      if (blue == '0')
+      { //links
+        if (mypixelx > 0)
+        {
+          mypixelx--;
+        }
+        else
+        {
+          mypixelx = 9;
+        }
+      }
 
-}
-}
+      if (blue == '7')
+      { //runter
+        if (mypixely > 0)
+        {
+          mypixely--;
+        }
+        else
+        {
+          mypixely = 14;
+        }
+      }
 
+      if (blue == '1')
+      { //neuesspiel
+      }
     }
+  }
 
   led.clear();
 
-  if(amihunter) {
- led.setPixelColor(pixel[thierpixelx][theirpixely],led.Color(10,0,0));
-  led.setPixelColor(pixel[mypixelx][mypixely],led.Color(0,70,0));
+  if (amihunter)
+  {
+    led.setPixelColor(pixel[thierpixelx][theirpixely], led.Color(10, 0, 0));
+    led.setPixelColor(pixel[mypixelx][mypixely], led.Color(0, 70, 0));
 
-  if(pixel[thierpixelx][theirpixely]==pixel[mypixelx][mypixely]) {
-    for(int y =0;y<15;y++) {
-      for(int x=0;x<15;x++) {
-          led.setPixelColor(pixel[x][y],led.Color(0,100,0)); //Hier warten auf weitere Angaben des Server wann ein neues Spiel gestartet wird
+    if (pixel[thierpixelx][theirpixely] == pixel[mypixelx][mypixely])
+    {
+      for (int y = 0; y < 15; y++)
+      {
+        for (int x = 0; x < 15; x++)
+        {
+          led.setPixelColor(pixel[x][y], led.Color(0, 100, 0)); //Hier warten auf weitere Angaben des Server wann ein neues Spiel gestartet wird
+        }
       }
-    } 
+      ingamewait = true;
+    }
   }
-  
-  }else{
-    led.setPixelColor(pixel[thierpixelx][theirpixely],led.Color(70,0,0));
-  led.setPixelColor(pixel[mypixelx][mypixely],led.Color(0,10,0));
+  else
+  {
+    led.setPixelColor(pixel[thierpixelx][theirpixely], led.Color(70, 0, 0));
+    led.setPixelColor(pixel[mypixelx][mypixely], led.Color(0, 10, 0));
 
-  if(pixel[thierpixelx][theirpixely]==pixel[mypixelx][mypixely]) {
-    for(int y =0;y<15;y++) {
-      for(int x=0;x<15;x++) {
-          led.setPixelColor(pixel[x][y],led.Color(100,0,0));
+    if (pixel[thierpixelx][theirpixely] == pixel[mypixelx][mypixely])
+    {
+      for (int y = 0; y < 15; y++)
+      {
+        for (int x = 0; x < 15; x++)
+        {
+          led.setPixelColor(pixel[x][y], led.Color(100, 0, 0));
+        }
       }
-    } 
+      ingamewait = true;
+    }
+
   }
-  }
-  
- 
+
   led.show();
 }
