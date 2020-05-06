3df1c32cc4d3b87c86c6dbe11df80d88f7d7485e
Coder246 <tim-arnold@outlook.de>
2020-05-06 13:04:41 +0200

    richtiges setzen der Variablen am Esp

--- PixelHunting/NodeMCU/Server/src/main.cpp
+++ PixelHunting/NodeMCU/Server/src/main.cpp
@@ -1,9 +1,92 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+const char *ssid = "WLANbridge";
+const char *password = "Pi-Server";
+const uint16_t port = 8090;
+const char *host = "192.168.1.83";
 
+int myposx = 0;
+int myposy = 0;
+int thierposx = 0;
+int thierposy = 0;
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+   WiFi.begin(ssid, password);
+}
+
+
+void settheirspos(int x, int y)
+{
+  Serial.print("o");
+  delay(2);
+  Serial.print("x");
+  delay(2);
+  Serial.print(x);
+  delay(2);
+  Serial.print("y");
+  delay(2);
+  Serial.print(y);
+  delay(2);
+  Serial.print("e");
+}
+
+void setmyspos(int x, int y)
+{
+  Serial.print("m");
+  delay(2);
+  Serial.print("x");
+  delay(2);
+  Serial.print(x);
+  delay(2);
+  Serial.print("y");
+  delay(2);
+  Serial.print(y);
+  delay(2);
+  Serial.print("e");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  String string="";
+  char c=2;
+  if (Serial.available())
+  
+  {
+
+    while (c != 'e')
+    {
+      delay(2);
+      
+      c = Serial.read();
+
+      string += c;
+    }
+
+    
+
+
+    
+    if (string.startsWith("x"))
+    {
+
+      int indexend = string.indexOf("e");
+      myposx = string.substring(1, indexend).toInt();
+    }
+
+    if (string.startsWith("y"))
+    {
+      
+      int indexend = string.indexOf("e");
+      myposy = string.substring(1, indexend).toInt();
+    }
+   
+    
+    //Handel input
+
+    string = "";
+  }
+
+  WiFiClient client;
+  //sethunter(false);
+  //settheirspos(9, 9);
+  delay(500);
 }
